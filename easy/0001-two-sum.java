cc9161bea38d9e9d4e4dd65c9a3c0f8fdbce8f46
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-04-30 18:27:15 +0530

    Add solution for Two Sum problem (LeetCode #1)

---
+++ easy/0001-two-sum.java
@@ -0,0 +1,24 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Date: 2025-04-30
+// Approach: HashMap to find complement of each element in the array.
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // Check if the complement exists in the map
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Store the current number and its index in the map
+            map.put(nums[i], i);
+        }
+        return new int[] {};  // If no solution found, return empty array
+    }
+}
