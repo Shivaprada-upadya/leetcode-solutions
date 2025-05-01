61efead57d7d520c16ca4eb3ec07fe3570344bd9
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-05-01 12:51:42 +0530

    Add solution for Reverse Integer problem (LeetCode #7)

---
+++ medium/0007-reverse-integer.java
@@ -0,0 +1,26 @@
+// Problem: Reverse Integer
+// Link: https://leetcode.com/problems/reverse-integer/
+// Date: 2025-05-01
+// Approach: Reverse digits using modulus and division, while handling integer overflow.
+
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0) 
+        {
+            int digit = x % 10;
+            x /= 10;
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; 
+            }
+            
+            reversed = reversed * 10 + digit;
+        }
+        
+        return reversed;
+        
+    }
+}
