f96024ee1beaec6311726a619c35357854d1d4fb
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-05-13 23:39:36 +0530

    Added Reverse Interger solution

--- medium/0007-reverse-integer.java
+++ medium/0007-reverse-integer.java
@@ -7,8 +7,7 @@ class Solution {
     public int reverse(int x) {
         int reversed = 0;
         while (x != 0) 
-        {
-            int digit = x % 10;
+        {        int digit = x % 10;
             x /= 10;
             if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
                 return 0;
