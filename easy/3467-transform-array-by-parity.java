4b2a4fe147af408c705f9239fa8369e5be2dac9f
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-02 23:05:29 +0530

    Added Solution for Tranform array by parity (3467)

---
+++ easy/3467-transform-array-by-parity.java
@@ -0,0 +1,36 @@
+// Problem: Transform the Array by Sorting and Counting Evens/Odds  
+//link: https://leetcode.com/problems/transform-array-by-parity/
+// Date: 2025-06-02  
+// Approach: Sort the array. Count number of even and odd numbers.  
+// Then, overwrite the array with 0s for evens and 1s for odds, maintaining counts only, not original values.
+
+class Solution {
+    public int[] transformArray(int[] nums) {
+        // Sorting the array
+        Arrays.sort(nums);
+
+        // Count the number of odd and even numbers
+        int odd = 0;
+        int even = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        // Fill the first 'even' elements with 0
+        int i;
+        for (i = 0; i < even; i++) {
+            nums[i] = 0;
+        }
+
+        // Fill the rest with 1
+        for (int j = i; j < nums.length; j++) {
+            nums[j] = 1;
+        }
+
+        return nums;
+    }
+}
