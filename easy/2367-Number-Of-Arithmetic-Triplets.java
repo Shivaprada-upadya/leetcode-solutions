dd23631ee090979c0df6a2f9fc160f9184c504c0
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-06 23:22:50 +0530

    Added solution for Number of Arithmetic Triplets -(2367)

--- easy/2367-Number-Of-Arithmetic-Triplets.java
+++ easy/2367-Number-Of-Arithmetic-Triplets.java
@@ -1,3 +1,22 @@
-public class 2367-Number-Of-Arithmetic-Triplets {
-    
+// Problem: Number of Arithmetic Triplets
+// Link: https://leetcode.com/problems/number-of-arithmetic-triplets/
+// Date: 2025-06-06
+// Approach: Brute force using three nested loops to check all triplets
+
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (nums[j] - nums[i] == diff) {
+                    for (int k = j + 1; k < nums.length; k++) {
+                        if (nums[k] - nums[j] == diff) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
 }
