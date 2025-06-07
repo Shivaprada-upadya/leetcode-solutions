86df68e85537a5cec9f64e1df06614bfe47b5a81
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-07 23:56:28 +0530

    Added File for the Solution -(3289)

--- easy/3289-Two-Sneaky-Numbers-of_Digitville.java
+++ easy/3289-Two-Sneaky-Numbers-of_Digitville.java
@@ -1,3 +1,9 @@
+// Problem: Count Pairs Whose Sum is Less than Target
+// Link: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+// Date: 2025-06-04
+// Approach: Using Brute-force method
+
+
 public class 3289-Two-Sneaky-Numbers-of_Digitville {
     
 }
