a274ea0487089e0e587d5492c3659fc3a083430d
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-09 23:47:19 +0530

    Added the Solution for Two Sneaky Numbers of Digitville Problem -(3289)

--- easy/3289-Two-Sneaky-Numbers-of_Digitville.java
+++ easy/3289-Two-Sneaky-Numbers-of_Digitville.java
@@ -7,7 +7,6 @@ class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         int n=nums.length;
         int count[]=new int[n];
-
         int result[] =new int[2];
         int i=0;
         for(int num:nums)
