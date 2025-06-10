6f68b4e944c19c3563459bce8f52c58f8cd20871
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-10 23:58:47 +0530

    Tried with the brute force approach -(1684)

--- easy/1684-count-the-number-of-consistent-strings.java
+++ easy/1684-count-the-number-of-consistent-strings.java
@@ -1,3 +1,18 @@
-public class 1684-count-the-number-of-consistent-strings {
-    
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int ans=0;
+
+        for(int i=0;i<words.length();i++)
+        {
+            while(int j=0;j<words[i].length;j++)
+            {
+                if(words.charAt(i)==allowed.charAt(i))
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+    }
 }
