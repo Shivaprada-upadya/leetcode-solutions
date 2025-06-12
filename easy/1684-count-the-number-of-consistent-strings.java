703260de739dd90a98fa73b76482b9c89e078c3b
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-12 23:16:58 +0530

    Tried with the brute force approach -(1684)

--- easy/1684-count-the-number-of-consistent-strings.java
+++ easy/1684-count-the-number-of-consistent-strings.java
@@ -9,8 +9,7 @@ class Solution {
                 {
                     ans++;
                 }            
-            }
-            
+            }         
         }            
         return ans;       
     }
