564b28d275a2135e8d6ee6cb001eafbc48c0cfd4
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-13 17:50:53 +0530

    Add the Solution of Number of 1 Bits -(0191)

--- easy/0191-Number-of-1-bits.java
+++ easy/0191-Number-of-1-bits.java
@@ -1,3 +1,12 @@
-public class 0191-Number-of-1-bits {
-    
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n>0)
+        {
+            count+=n&1;
+            n=n>>>1;
+        }
+
+        return count;        
+    }
 }
