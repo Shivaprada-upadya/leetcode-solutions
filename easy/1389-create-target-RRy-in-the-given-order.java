826785a5d253cad7cc0f725d18b7d13a86ad1e4e
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-14 23:36:18 +0530

    Added solution for create target array in the given order

--- easy/1389-create-target-RRy-in-the-given-order.java
+++ easy/1389-create-target-RRy-in-the-given-order.java
@@ -1,3 +1,15 @@
-public class 1389-create-target-RRy-in-the-given-order {
-    
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int n=nums.length;
+        int result[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int temp=nums[i];
+            nums[i]=nums[index[i]];
+            nums[index[i]]=temp;
+        }
+
+
+        return nums;
+    }
 }
