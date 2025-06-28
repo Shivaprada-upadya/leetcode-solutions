366463bf7f3fb46330b2718b1228e233ffef014c
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-29 01:09:13 +0530

    Simply Added Math Formula

---
+++ easy/2769-Find-The-Maximum-Achievable-Number.java
@@ -0,0 +1,10 @@
+// Problem: Find the Maximum Achievable Number
+// Link: https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Date: 2025-06-29
+// Approach: Using num+(t*2) formula
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + (t*2);
+    }
+}
