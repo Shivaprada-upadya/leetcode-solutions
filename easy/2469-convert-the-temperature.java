97cf2cabbbf34a3198cc00497042ec8b467e392e
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-29 17:13:24 +0530

    added simple temperature formula

--- easy/2469-convert-the-temperature.java
+++ easy/2469-convert-the-temperature.java
@@ -1,3 +1,10 @@
-public class 2469-convert-the-temperature {
-    
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin=celsius + 273.15;
+        double fahrenheit=celsius * 1.80 + 32.00;
+        double ans[]=new double[2];
+        ans[0]=kelvin;
+        ans[1]=fahrenheit;       
+        return ans;        
+    }
 }
