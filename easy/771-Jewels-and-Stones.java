61c61472a30b1b3f4e54f556265c408e6427088e
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-01 20:48:02 +0530

    Update the description of the problem with the link,etc

--- easy/771-Jewels-and-Stones.java
+++ easy/771-Jewels-and-Stones.java
@@ -1,5 +1,7 @@
-
-
+// Problem: 771. Jewels and Stones 
+// Link: https://leetcode.com/problems/jewels-and-stones/
+// Date: 2025-07-01
+// Approach: Using nested for loop
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count=0;
