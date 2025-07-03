34b53225ec72e274bcbddf39bad532e9ddc01d83
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-03 23:20:15 +0530

    Updated the Problem link

--- medium/1393-capital-gain/-loss.sql
+++ medium/1393-capital-gain/-loss.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+--Problem link: https://leetcode.com/problems/capital-gainloss/
 
 SELECT stock_name, SUM(if(operation='Buy',-price,price)) AS capital_gain_loss 
 FROM Stocks
