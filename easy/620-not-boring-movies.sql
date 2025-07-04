9fb50b7ef5c1f48d313766d08dc6319cb0c35ace
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-04 23:13:49 +0530

    Added Solution for sql problem

---
+++ easy/620-not-boring-movies.sql
@@ -0,0 +1,7 @@
+-- Problem link: https://leetcode.com/problems/not-boring-movies/
+-- Write your MySQL query statement below
+
+SELECT id,movie,description,rating
+FROM Cinema
+WHERE MOD(id,2)=1 AND description!='boring'
+ORDER BY rating DESC;
