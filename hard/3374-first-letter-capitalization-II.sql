0a93ab340e0cdd61aad8387a89572f9f00b0f688
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-07 23:46:54 +0530

    Updated problem link

--- hard/3374-first-letter-capitalization-II.sql
+++ hard/3374-first-letter-capitalization-II.sql
@@ -1,4 +1,5 @@
-/* Write your PL/SQL query statement below */
+/* Problem link: https://leetcode.com/problems/first-letter-capitalization-ii/
+ Write your PL/SQL query statement below */
 
 select content_id, content_text as original_text,
        initcap(LOWER(content_text)) as converted_text
