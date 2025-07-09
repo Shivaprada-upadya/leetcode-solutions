7b1aa7ab7dc77d2e3b653ddc48bbcecc18810831
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-10 00:02:43 +0530

    Added solution for Add two Integers (java) (1484)

---
+++ easy/2235-Add-Two-Integers.java
@@ -0,0 +1,10 @@
+// Problem: Add Two Integer
+// Link: https://leetcode.com/problems/add-two-integers/
+// Date: 2025-07-09
+// Approach: Using simple addition
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;       
+    }
+}
