9175f480d9683171a5aec72f736d7d83e99375ff
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-10 21:36:31 +0530

    Updated and add the Problem description

--- easy/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ easy/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,3 +1,8 @@
+// Problem:  How Many Numbers Are Smaller Than the Current Number
+// Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+// Date: 2025-07-10
+// Approach: using Brute Force Method
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n=nums.length;
