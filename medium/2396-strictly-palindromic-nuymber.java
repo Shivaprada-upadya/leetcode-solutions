61b885ab21a2e051b89bfb8e30a7196f6f555e5f
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-13 22:36:58 +0530

    Updated Problem description

--- medium/2396-strictly-palindromic-nuymber.java
+++ medium/2396-strictly-palindromic-nuymber.java
@@ -1,3 +1,8 @@
+// Problem: Strictly Palindromic Number
+// Link: https://leetcode.com/problems/strictly-palindromic-number/
+// Date: 2025-07-13
+// Approach: return false
+
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
         return false;       
