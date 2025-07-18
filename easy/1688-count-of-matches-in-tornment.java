705ff145845b8455cc17653b78c6f7abfd56fcd8
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-18 23:46:37 +0530

    Updated the problem descriptions

--- easy/1688-count-of-matches-in-tornment.java
+++ easy/1688-count-of-matches-in-tornment.java
@@ -1,3 +1,8 @@
+// Problem: Count of Matches in Tournament
+// Link: https://leetcode.com/problems/count-of-matches-in-tournament/
+// Date: 2025-07-18
+// Approach: return n-1
+
 class Solution {
     public int numberOfMatches(int n) {
         return n-1;
