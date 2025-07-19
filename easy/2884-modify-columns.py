52425c677c567abfe33835e7985cb398921cc680
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-19 18:07:22 +0530

    Updated the Problem Descritions

--- easy/2884-modify-columns.py
+++ easy/2884-modify-columns.py
@@ -1,6 +1,10 @@
-
+# Problem: 2884: Modify Columns
+# Link: https://leetcode.com/problems/modify-columns/
+# Approach: By Accessing the column and modify the column
+# Date : 19-07-2025
 
 import pandas as pd
+
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
    employees["salary"]*=2
    return employees
