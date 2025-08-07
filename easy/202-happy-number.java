c36b2ecb6e3945da6ee6e4e9ac253a7dae37cbf3
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-07 21:53:55 +0530

    Added - Happy number program

---
+++ easy/202-happy-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int SumOfSquare(int num)
+    {
+        int sum=0;
+        int temp=num;
+        while(temp>0)
+        {
+            int ld=temp%10;
+            int sq=ld*ld;
+            sum=sum+sq;
+            temp=temp/10;           
+        }
+        return sum;
+
+    }
+    
+    public boolean isHappy(int n) {
+        int num=n;
+        while(num!=1 && num!=4)
+        {
+            num=SumOfSquare(num);          
+        }
+        if(num==1)
+            return true;
+        else
+            return false;
+    
+    }
+}
