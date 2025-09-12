719c17abb774989ffb6e008b49910a8e074b07e2
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-09-12 21:28:40 +0530

    Added - Add string program

---
+++ easy/415-add-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i=num1.length()-1, j=num2.length()-1;
+
+        String res="";
+        int carry=0;
+
+        while(i>=0 || j>=0)
+        {
+            int a=0, b=0;
+            if(i>=0)
+            {
+                a=num1.charAt(i)-48;
+                i--;
+            }
+
+            if(j>=0)
+            {
+                b=num2.charAt(j)-48;
+                j--;
+            }
+
+            int temp=a+b+carry;
+            res=(temp%10)+res;
+            carry=temp/10;
+
+        }
+
+        if(carry>0)
+        {
+            res=carry+res;
+        }
+        return res;
+    }
+}
